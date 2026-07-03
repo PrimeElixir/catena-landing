b06551a9a57fbd21d85f6dfe05f5fd1fa43caf57
Brian Mcleod <brian.mcleod@primeelixirco.com>
2026-07-03 01:13:31 -0400

    fix: add how-it-works ID to OperatingModel section to fix header navigation

--- src/components/OperatingModel.tsx
+++ src/components/OperatingModel.tsx
@@ -35,7 +35,7 @@ export default function OperatingModel() {
   }, []);
 
   return (
-    <section className="py-32 px-6">
+    <section id="how-it-works" className="py-32 px-6">
       <div className="max-w-6xl mx-auto flex flex-col lg:flex-row items-center gap-16 lg:gap-24">
         
         {/* Left: The Circular Animation Diagram */}
